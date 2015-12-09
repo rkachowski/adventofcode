f9393baa89435ccf3c2f1988e92dde749a59c490
Donald Hutchison <git@toastymofo.net>
2015-12-09 19:40:51 +0100

    Simplify.

--- d9/solution.rb
+++ d9/solution.rb
@@ -1,44 +1,31 @@
 input = File.read "input"
 
-#cities = input.each_line.map{|l| words = l.split(" "); [words[0],words[2]]}.flatten.uniq
+cities = input.each_line.map{|l| words = l.split(" "); [words[0],words[2]]}.flatten.uniq
 
-class Node
-  attr_accessor :name, :edges
-  def initialize name
-    @name=name
-    @edges = {}
-  end
-end
-
-class Graph
-  attr_reader :nodes
-
-  def initialize
-      @nodes = {}
-  end
-
-  def get_node name
-    unless @nodes[name]
-      @nodes[name] = Node.new name
-    end
-    @nodes[name]
-  end
-
-  def add_edge name1, name2, distance
-    node1 = get_node name1
-    node2 = get_node name2
-
-    node1.edges[name2] = distance
-    node2.edges[name1] = distance
-  end
-end
-
-
-graph = Graph.new
+graph = {}
 input.each_line do |line|
   words = line.split(" ")
-  graph.add_edge words[0], words[2], words[4].to_i
+  city1, city2, distance = words[0], words[2], words[4].to_i
 
+  graph[city1] ||= {}
+  graph[city1][city2] = distance
+  graph[city2] ||= {}
+  graph[city2][city1] = distance
 end
 
-puts graph.nodes
+paths = cities.permutation.map{|c| c.join "->"}
+distances = {}
+paths.each do |path|
+  cities_in_path = path.split "->"
+
+  distance = 0
+  cities_in_path.each_with_index do |cp,i|
+    next if i == 0
+    c1, c2 = cities_in_path[i-1], cities_in_path[i]
+    distance += graph[c1][c2]
+  end
+  distances[path] = distance
+end
+
+puts "shortest distance #{ distances.values.min} : #{distances.key(distances.values.min)}"
+puts "longest distance #{distances.values.max} : #{distances.key(distances.values.max)}"
